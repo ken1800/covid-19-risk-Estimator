ae5a312f2936b91a785e2064a11fbd8855f19a7f
kenneth irungu <kennethirungu1800@gmail.com>
2020-06-24 22:48:43 +0300

    Updates

---
+++ precache-manifest.58ca0d863ee4e426159e20a0b253ac2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c816e994d9c8f5644ad84f04b1e5c2f0",
+    "url": "/estimator/index.html"
+  },
+  {
+    "revision": "092b6121f0207655d489",
+    "url": "/estimator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "26745ad71318b4b8d6ed",
+    "url": "/estimator/static/css/main.14aaa9f2.chunk.css"
+  },
+  {
+    "revision": "092b6121f0207655d489",
+    "url": "/estimator/static/js/2.83a2a49e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/estimator/static/js/2.83a2a49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26745ad71318b4b8d6ed",
+    "url": "/estimator/static/js/main.83e42b5b.chunk.js"
+  },
+  {
+    "revision": "c19971cbfcc457a311b8",
+    "url": "/estimator/static/js/runtime-main.68e4d606.js"
+  }
+]);
--- src/redux/reducers/message/messageSelector.jsx
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-const selectMessage = (state) => state.message;
-
-export const selectedMessages = createSelector(
-  [selectMessage],
-  (message) => message.messo
-);
